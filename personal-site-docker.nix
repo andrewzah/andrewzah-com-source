5b7868b12527c33339df47523da333237a455f54
Andrew Zah <zah@andrewzah.com>
2024-06-24 18:23:40 +0900

    create basic dockerfile shell with caddy

---
+++ personal-site-docker.nix
@@ -0,0 +1,26 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/dd457de7e08c6d06789b1f5b88fc9327f4d96309.tar.gz";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+  caddyfile = pkgs.writeTextDir "/etc/caddy/Caddyfile" (builtins.readFile ./Caddyfile);
+in
+rec {
+  img = pkgs.dockerTools.buildImage {
+    name = "docker.io/andrewzah/personal_site";
+    tag = "latest";
+
+    copyToRoot = pkgs.buildEnv {
+      name = "img-root";
+      paths = with pkgs; [
+        caddy
+        caddyfile
+
+        #(pkgs.callPackage ./personal-site.nix {})
+      ];
+    };
+
+    config = {
+      Cmd = [ "caddy" "run" "--config" "/etc/caddy/Caddyfile" "--adapter"  "caddyfile" ];
+    };
+  };
+}
